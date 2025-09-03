227c0cf4f682b7726fd5e3f7ee7365ef301b0684
joshuabanjopro <joshuabanjopro@gmail.com>
2025-09-03 14:37:47 +0100

    Consumed JSON data from external API and then used data to populate Players table in db

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,39 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require "json"
+require "rest-client"
+require "pp"
+
+puts "Cleaning database..."
+Player.destroy_all
+
+puts "Populating Players table with sourced from API..."
+
+response = RestClient.get "http://rest.nbaapi.com/api/PlayerDataTotals/season/2010"
+arr_players = JSON.parse(response)
+
+puts("Testing...")
+pp(arr_players[0])
+
+arr_players.each do |player|
+  puts("Inserting a new Player into the Players table...")
+  player_new = Player.new()
+  player_new.name = player["playerName"]
+  player_new.position = player["position"]
+  player_new.assists = player["assists"].to_i
+  player_new.steals = player["steals"].to_i
+  player_new.blocks = player["blocks"].to_i
+  player_new.points = player["points"].to_s
+  player_new.team = player["team"]
+  player_new.season = player["season"].to_i
+
+  player_new.save
+  if player_new
+    puts("A new Player has been added to the Players table successfully")
+  else
+    puts("Houston, we have a problem...")
+  end
+
+  puts("Finished!")
+end
